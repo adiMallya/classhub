99d69815677720dd5c5e7b10d6b750ba349a0529
adiMallya <adimallya99@gmail.com>
2023-10-23 19:52:26 +0530

    refactor: added breakpoints

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -32,8 +32,17 @@ const components = {
     }
 };
 
+const breakpoints = {
+    base: "0em", // 0px
+    sm: "30em", // ~480px 
+    md: "48em", // ~768px
+    lg: "62em", // ~992px
+    xl: "80em", // ~1280px
+};
+
 export const theme = extendTheme({
     fonts,
     styles,
-    components
+    components,
+    breakpoints
 });
