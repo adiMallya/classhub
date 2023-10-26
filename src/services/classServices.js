6d3a7b11d708a14a529bb9787aad04cecc46275c
adiMallya <adimallya99@gmail.com>
2023-10-27 00:35:21 +0530

    refactor: toast messages for error

--- src/services/classServices.js
+++ src/services/classServices.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { BASE_URL } from "src/utils";
+import toast from "react-hot-toast";
 
 const getClassesFromServer = async () => {
     const response = await axios.get(`${BASE_URL}/class`);
     if (response.data.success) {
         return response.data.data;
     } else {
+        toast.error("Failed to fetch data.");
         throw new Error(response.data.error);
     }
 }
@@ -15,6 +17,7 @@ const getMembersFromClass = async (classId, type) => {
     if (response.data.success) {
         return response.data.data;
     } else {
+        toast.error("Class does not exist.");
         throw new Error(response.data.error);
     }
 }
--- src/services/studentServices.js
+++ src/services/studentServices.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { BASE_URL } from "src/utils";
+import toast from "react-hot-toast";
 
 const getStudentsFromServer = async () => {
     const response = await axios.get(`${BASE_URL}/students`);
     if (response.data.success) {
         return response.data.data;
     } else {
+        toast.error("Failed to fetch data.");
         throw new Error(response.data.error);
     }
 }
@@ -15,6 +17,7 @@ const postStudentToServer = async (studentData) => {
     if (response.data.success) {
         return response.data.data;
     } else {
+        toast.error("Failed to add student. Check your inputs.");
         throw new Error(response.data.error);
     }
 }
@@ -24,6 +27,7 @@ const updateStudentInServer = async (studentId, fieldsToUpdate) => {
     if (response.data.success) {
         return response.data.data;
     } else {
+        toast.error("Failed to update student. Check your inputs.");
         throw new Error(response.data.error);
     }
 }
@@ -31,6 +35,7 @@ const updateStudentInServer = async (studentId, fieldsToUpdate) => {
 const deleteStudentFromServer = async (studentId) => {
     const response = await axios.delete(`${BASE_URL}/students/${studentId}`);
     if (response.status !== 204) {
+        toast.error("Student does not exist.");
         throw new Error(response.data.error);
     }
 }
