caa805faf4684f1fdf37968157831fe1dd97860f
adiMallya <adimallya99@gmail.com>
2023-10-26 18:55:24 +0530

    feat: Class selector

--- src/features/index.js
+++ src/features/index.js
@@ -2,3 +2,5 @@ export * from "./students/studentsSlice";
 export * from "./classes/classSlice";
 export { StudentList } from "./students/components/StudentList";
 export { StudentModal } from "./students/components/StudentModal";
+export { ClassSelector } from "./classes/components/ClassSelector";
+export { ClassGrid } from "./classes/components/ClassGrid";
