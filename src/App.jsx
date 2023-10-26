60a919ee0c156a8e4325777f6c03c914c2854f38
adiMallya <adimallya99@gmail.com>
2023-10-27 00:09:46 +0530

    refactor: fetch classes, students on load

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import { SchoolView, StudentsView, ClassView } from "src/pages";
 import { Toaster } from "react-hot-toast";
+import { fetchClassesAsync, fetchStudentsAsync } from "features";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchClassesAsync());
+    dispatch(fetchStudentsAsync());
+  }, []);
+
   return (
     <>
       <Toaster position="bottom-right" reverseOrder={false} />
