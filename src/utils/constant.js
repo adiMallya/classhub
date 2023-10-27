27883c6987115f6625b4d43bcfdd457efae19dbe
adiMallya <adimallya99@gmail.com>
2023-10-27 19:32:17 +0530

    add: subjects

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,15 @@
 export const BASE_URL = "https://classhub-server.slaypoint.repl.co/api/v1";
+
+export const SUBJECTS = [
+    { key: 'mathematics', value: 'Mathematics' },
+    { key: 'physics', value: 'Physics' },
+    { key: 'chemistry', value: 'Chemistry' },
+    { key: 'biology', value: 'Biology' },
+    { key: 'history_civics', value: 'History/Civics' },
+    { key: 'geography', value: 'Geography' },
+    { key: 'computer_science', value: 'Computer Science' },
+    { key: 'english', value: 'English' },
+    { key: 'hindi', value: 'Hindi' },
+    { key: 'french', value: 'French' },
+    { key: 'physical_development', value: 'Physical Development' }
+];
