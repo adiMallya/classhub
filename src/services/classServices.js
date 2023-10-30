c0d97af12b08c73b46cfc7b2bd6b7125099a6971
adiMallya <adimallya99@gmail.com>
2023-10-30 18:30:22 +0530

    fix: error handling resolved

--- src/services/classServices.js
+++ src/services/classServices.js
@@ -3,22 +3,25 @@ import { BASE_URL } from "src/utils";
 import toast from "react-hot-toast";
 
 const getClassesFromServer = async () => {
-    const response = await axios.get(`${BASE_URL}/class`);
-    if (response.data.success) {
-        return response.data.data;
-    } else {
+    try {
+        const response = await axios.get(`${BASE_URL}/class`);
+        if (response.data.success) {
+            return response.data.data;
+        }
+    } catch (error) {
         toast.error("Failed to fetch data.");
-        throw new Error(response.data.error);
+
     }
 }
 
 const getMembersFromClass = async (classId, type) => {
-    const response = await axios.get(`${BASE_URL}/class/${classId}/members?type=${type}`);
-    if (response.data.success) {
-        return response.data.data;
-    } else {
-        toast.error("Class does not exist.");
-        throw new Error(response.data.error);
+    try {
+        const response = await axios.get(`${BASE_URL}/class/${classId}/members?type=${type}`);
+        if (response.data.success) {
+            return response.data.data;
+        }
+    } catch ({ response }) {
+        toast.error(response.data.error);
     }
 }
 
--- src/services/studentServices.js
+++ src/services/studentServices.js
@@ -3,40 +3,43 @@ import { BASE_URL } from "src/utils";
 import toast from "react-hot-toast";
 
 const getStudentsFromServer = async () => {
-    const response = await axios.get(`${BASE_URL}/students`);
-    if (response.data.success) {
-        return response.data.data;
-    } else {
+    try {
+        const response = await axios.get(`${BASE_URL}/students`);
+        if (response.data.success) {
+            return response.data.data;
+        }
+    } catch (error) {
         toast.error("Failed to fetch data.");
-        throw new Error(response.data.error);
     }
 }
 
 const postStudentToServer = async (studentData) => {
-    const response = await axios.post(`${BASE_URL}/students`, { ...studentData });
-    if (response.data.success) {
-        return response.data.data;
-    } else {
-        toast.error("Failed to add student. Check your inputs.");
-        throw new Error(response.data.error);
+    try {
+        const response = await axios.post(`${BASE_URL}/students`, { ...studentData });
+        if (response.data.success) {
+            return response.data.data;
+        }
+    } catch ({ response }) {
+        toast.error(response.data.error);
     }
 }
 
 const updateStudentInServer = async (studentId, fieldsToUpdate) => {
-    const response = await axios.post(`${BASE_URL}/students/${studentId}`, { ...fieldsToUpdate });
-    if (response.data.success) {
-        return response.data.data;
-    } else {
-        toast.error("Failed to update student. Check your inputs.");
-        throw new Error(response.data.error);
+    try {
+        const response = await axios.post(`${BASE_URL}/students/${studentId}`, { ...fieldsToUpdate });
+        if (response.data.success) {
+            return response.data.data;
+        }
+    } catch ({ response }) {
+        toast.error(response.data.error);
     }
 }
 
 const deleteStudentFromServer = async (studentId) => {
-    const response = await axios.delete(`${BASE_URL}/students/${studentId}`);
-    if (response.status !== 204) {
-        toast.error("Student does not exist.");
-        throw new Error(response.data.error);
+    try {
+        await axios.delete(`${BASE_URL}/students/${studentId}`);
+    } catch ({ response }) {
+        toast.error(response.data.error);
     }
 }
 
